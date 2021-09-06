903113bcf94290e6e474cc826c32ce2ba17d258f
mostafaashraf92 <most.ashraf92@gmail.com>
2021-09-06 14:31:33 +0200

    Add new response

--- index.js
+++ index.js
@@ -24,20 +24,121 @@ const typeDefs = gql`
 const mediaItems = [
   {
     id: '0',
-    name: 'Sintel',
-    url : 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+    title: 'Sintel',
+    description: 'A video',
+    duration: '630',
+    images: imagesList,
+    streams: [
+      {
+         format:'hls',
+         container:'cmaf',
+         fps:59.97,
+         drm:'cbcs',
+         segmentDuration:2,
+         videoInfo:[
+            {
+               id:1,
+               codec:'h265',
+               bitrate:400000,
+               width:1920,
+               height:1080,
+               bitDepth:8
+            }
+         ],
+         audioInfo:[
+            {
+               id:2,
+               codec:'aac',
+               language:'en',
+               bitRate:4000,
+               channels:6
+            }
+         ],
+         textInfo:[
+            {
+               id:'CC1',
+               type:'cea-608',
+               language:'en'
+            }
+         ],
+         sources:[
+            {
+               cdn:'Akamai',
+               url:'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8'
+            },
+            {
+               cdn:'Fastly',
+               url:'https://some.fastly.com/blah.m3u8'
+            }
+         ]
+      },
+      {
+         format:'dash',
+         container:'cmaf',
+         fps:29.97,
+         drm:'clear',
+         segmentDuration:2,
+         videoInfo:[
+            {
+               id:1,
+               codec:'h264',
+               bitrate:300000,
+               width:1280,
+               height:720,
+               bitDepth:8
+            }
+         ],
+         audioInfo:[
+            {
+               id:2,
+               codec:'aac',
+               language:'en',
+               bitRate:4000,
+               channels:2
+            }
+         ],
+         textInfo:[
+            {
+               id:'CC1',
+               type:'cea-608',
+               language:'en'
+            }
+         ],
+         sources:[
+            {
+               cdn:'Akamai',
+               url:'https: //some.akamai.com/blah.mpd'
+            },
+            {
+               cdn:'Fastly',
+               url:'https: //some.fastly.com/blah.mpd'
+            }
+         ]
+      }
+   ]
   },
   {
     id: '1',
-    name: 'Big Buck Bunny',
+    title: 'Big Buck Bunny',
     url : 'https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8',
   },
   {
     id: '3',
-    name: 'Bip Bop',
+    title: 'Bip Bop',
     url : 'http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8',
   },
 ];
+
+const imagesList = [
+  {
+    url: 'http://placehold.it/1280x720?text=Poster',
+    type: 'poster',
+    description: 'A video',
+    width: '1280',
+    images: '720'
+  }
+];
+
 
 const resolvers = {
   Query: {
